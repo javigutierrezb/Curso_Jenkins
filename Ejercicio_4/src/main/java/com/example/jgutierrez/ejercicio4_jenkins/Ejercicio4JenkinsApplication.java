de4c38fdf311219edbda788ae1789527a46affd9
Javier Gutiérrez <72932950+javigutierrezb@users.noreply.github.com>
2023-05-31 10:26:30 +0200

    Archivos maven
    Añadiendo archivos para proyecto maven con mariadb y mysql

---
+++ Ejercicio_4/src/main/java/com/example/jgutierrez/ejercicio4_jenkins/Ejercicio4JenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jgutierrez.ejercicio4_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio4JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio4JenkinsApplication.class, args);
+	}
+
+}
