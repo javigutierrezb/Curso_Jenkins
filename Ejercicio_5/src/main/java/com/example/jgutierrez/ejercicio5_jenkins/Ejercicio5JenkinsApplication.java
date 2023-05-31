0047d6fc998364bb9b432e0a107204a93c9b57de
Javier Gutiérrez <72932950+javigutierrezb@users.noreply.github.com>
2023-05-31 18:03:18 +0200

    Add files via upload

---
+++ Ejercicio_5/src/main/java/com/example/jgutierrez/ejercicio5_jenkins/Ejercicio5JenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jgutierrez.ejercicio5_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio5JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio5JenkinsApplication.class, args);
+	}
+
+}
