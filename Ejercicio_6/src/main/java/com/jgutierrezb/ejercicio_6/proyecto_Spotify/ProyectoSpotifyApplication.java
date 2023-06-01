9bf42755e1b9388bbeee67adfeef03ebefab6f47
Javier Gutiérrez <72932950+javigutierrezb@users.noreply.github.com>
2023-06-01 12:40:18 +0200

    Add files via upload

---
+++ Ejercicio_6/src/main/java/com/jgutierrezb/ejercicio_6/proyecto_Spotify/ProyectoSpotifyApplication.java
@@ -0,0 +1,13 @@
+package com.jgutierrezb.ejercicio_6.proyecto_Spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpotifyApplication.class, args);
+	}
+
+}
---
+++ Ejercicio_6/src/test/java/com/jgutierrezb/ejercicio_6/proyecto_Spotify/ProyectoSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgutierrezb.ejercicio_6.proyecto_Spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
